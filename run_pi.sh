81fcb809705bdd127748daaf1664f0a7ee1f796e
Jacob Jewett <jacoblj3333@gmail.com>
2023-11-06 00:50:03 -0700

    separate setup commands into setup_pi.sh

--- run_pi.sh
+++ run_pi.sh
@@ -1,45 +1,15 @@
 #!/usr/bin/env bash
-ATSC_DIR=.
-ENV_DIR=$ATSC_DIR/env/debian
-INTERPRETER_DIR=$ENV_DIR/bin
-INTERPRETER=python3
-INTERPRETER_PATH=$INTERPRETER_DIR/$INTERPRETER
-ATSC_PID_PATH=$ATSC_DIR/atsc.pid
+source ./env_pi.sh
 
 if ! [ -x "$INTERPRETER_PATH" ];
 then
   echo "Interpreter missing"
-
-  if ! [ -d "$INTERPRETER_DIR" ];
-  then
-    echo "Creating virtual environment"
-
-    $INTERPRETER -m venv $ENV_DIR
-
-    if [ $? -ne 0 ];
-    then
-      exit 10000
-    fi
-  fi
-
-  echo "Updating pip and installing pip-tools"
-  $INTERPRETER_PATH -m pip install --upgrade pip
-  $INTERPRETER_PATH -m pip install pip-tools
+  source ./setup_pi.sh
 
   if [ $? -ne 0 ];
   then
-    exit 10001
+    exit 10002
   fi
-
-  echo "Activating environment"
-  source "$INTERPRETER_DIR/activate"
-fi
-
-pip-sync
-
-if [ $? -ne 0 ];
-then
-  exit 10002
 fi
 
 $INTERPRETER_PATH -OO -m atsc.main --pid "$ATSC_PID_PATH" "$ATSC_DIR/configs/rpi.json" &
---
+++ setup_pi.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+source ./env_pi.sh
+
+if ! [ -x "$INTERPRETER_PATH" ];
+then
+  if ! [ -d "$INTERPRETER_DIR" ];
+  then
+    echo "Creating virtual environment"
+
+    $INTERPRETER -m venv $ENV_DIR
+
+    if [ $? -ne 0 ];
+    then
+      exit 10000
+    fi
+  fi
+
+  echo "Updating pip and installing pip-tools"
+  $INTERPRETER_PATH -m pip install --upgrade pip
+  $INTERPRETER_PATH -m pip install pip-tools
+
+  if [ $? -ne 0 ];
+  then
+    exit 10001
+  fi
+fi
+
+source "$INTERPRETER_DIR/activate"
+pip-sync
