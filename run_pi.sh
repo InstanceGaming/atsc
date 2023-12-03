71483ae41b460a7f5e5ef81a37dcca3afc41e2a5
Jacob Jewett <jacoblj3333@gmail.com>
2023-12-02 22:29:50 -0700

    missing exit in run_pi.sh

--- run_pi.sh
+++ run_pi.sh
@@ -4,6 +4,7 @@ source ./env_pi.sh
 if ! [ -x "$INTERPRETER_PATH" ];
 then
   echo "Interpreter missing"
+  exit 10002
 fi
 
 $INTERPRETER_PATH -OO -m atsc.main --pid "$ATSC_PID_PATH" "$ATSC_DIR/configs/rpi.json" &
