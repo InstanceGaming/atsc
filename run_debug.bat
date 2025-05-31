a839ad3a096063bab06b470104962910ff8f3855
Jacob Jewett <jacoblj3333@gmail.com>
2025-05-30 23:51:58 -0600

    run_debug.bat logging levels

--- run_debug.bat
+++ run_debug.bat
@@ -1,2 +1,2 @@
 @echo off
-.\env\win32\scripts\python -OO -m atsc.main -l "FIELDS,WARNING;stderr=ERROR" configs/dev1.json
+.\env\win32\scripts\python -OO -m atsc.main -l "FIELDS,WARNING;stderr=ERROR;file=FIELDS,ERROR" configs/dev1.json
